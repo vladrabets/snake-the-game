5bbc38961bbd4ddba4344245557c61f685dbd302
Rabets Vlad <vladrabets46@gmai.com>
2021-05-16 16:00:51 +0300

    fix

--- script.js
+++ script.js
@@ -144,13 +144,13 @@ document.addEventListener('keydown', direction);
 let dir;
 
 function direction(event) {
-    if(event.keyCode === 37 || event.keyCode === 65 && dir !== "right")
+    if((event.keyCode === 37 || event.keyCode === 65) && dir !== "right")
         dir = "left";
-    else if(event.keyCode === 38 || event.keyCode === 87 && dir !== "down")
+    else if((event.keyCode === 38 || event.keyCode === 87) && dir !== "down")
         dir = "up";
-    else if(event.keyCode === 39 || event.keyCode === 68 && dir !== "left")
+    else if((event.keyCode === 39 || event.keyCode === 68) && dir !== "left")
         dir = "right";
-    else if(event.keyCode === 40 || event.keyCode === 83 && dir !== "up")
+    else if((event.keyCode === 40 || event.keyCode === 83) && dir !== "up")
         dir = "down";
 }
 
